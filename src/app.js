591e2f59b8c81e6f832cccfac0aec71f8ad6acbb
sabilimaulana <spacysbl.learn@gmail.com>
2021-08-21 14:06:19 +0700

    Membuat model Student

--- src/app.js
+++ src/app.js
@@ -1,8 +1,12 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 const port = process.env.PORT || 8080;
 const cors = require("cors");
+const Student = require("./models/student");
+
+require("dotenv").config();
 
 app.use(express.json());
 app.use(cors());
@@ -10,6 +14,29 @@ app.use("/", (req, res) => {
   res.send("Node Js x MongoDB");
 });
 
-app.listen(port, () => {
-  console.log(`Student app running on http://localhost:${port}`);
-});
+const { MONGODB_LOCAL_DBNAME } = process.env;
+const uri = `mongodb://127.0.0.1:27017/${MONGODB_LOCAL_DBNAME}`;
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+};
+
+mongoose.set("useFindAndModify", true);
+mongoose
+  .connect(uri, options)
+  .then(() => {
+    app.listen(port, async () => {
+      console.log(`Student app running on http://localhost:${port}`);
+
+      // const newStudentModel = new Student({
+      //   name: "Sabili",
+      //   email: "sabili@gmail.com",
+      // });
+
+      // const newStudent = await newStudentModel.save();
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
---
+++ src/models/student.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const studentSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Student = mongoose.model("Student", studentSchema);
+
+module.exports = Student;
